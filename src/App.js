9ad673adc77a199efc1a17f0451a9101f9de460a
Petr Zalizko <petrzalizko@Petrs-MacBook-Air.local>
2025-03-03 13:29:17 +0100

    Added view from Figma, used plugin Figma to Code, code is not cleaned

--- src/App.js
+++ src/App.js
@@ -3,26 +3,68 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Figma
-        </a>
-        <a
-          className="vika-petya"
-        >
-          ВИКА + ПЕТЯ = &#10084;
-        </a>
-      </header>
+    <div>
+      <div style={{width: 393, height: 852, position: 'relative', background: 'white'}}>
+  <div style={{width: 111, left: 141, top: 35, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Sign In</div>
+  <div style={{width: 337, height: 69, left: 28, top: 88, position: 'absolute'}}>
+    <div style={{width: 337, height: 69, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 50, border: '1px #B9B1B1 solid'}} />
+    <div style={{width: 93.01, left: 36.24, top: 25, position: 'absolute', color: '#B9B1B1', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Email</div>
+  </div>
+  <div style={{width: 337, height: 69, left: 28, top: 426, position: 'absolute'}}>
+    <div style={{width: 337, height: 69, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 50, border: '1px #B9B1B1 solid'}} />
+    <div data-svg-wrapper style={{left: 16, top: 16, position: 'absolute'}}>
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_8_37)">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.62031 20C8.62031 18.701 8.83593 17.4553 9.22125 16.2872L2.48062 11.14C1.16687 13.8072 0.426872 16.8128 0.426872 20C0.426872 23.1847 1.16625 26.1881 2.47781 28.8538L9.21468 23.6966C8.83312 22.5338 8.62031 21.2928 8.62031 20Z" fill="#FBBC05"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M20.4553 8.18188C23.2775 8.18188 25.8266 9.18188 27.8294 10.8181L33.6556 5C30.1053 1.90906 25.5534 0 20.4553 0C12.5403 0 5.73781 4.52625 2.48062 11.14L9.22093 16.2872C10.7741 11.5728 15.2012 8.18188 20.4553 8.18188Z" fill="#EA4335"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M20.4553 31.8181C15.2016 31.8181 10.7744 28.4272 9.22125 23.7128L2.48062 28.859C5.73781 35.4737 12.5403 40 20.4553 40C25.3403 40 30.0044 38.2653 33.5047 35.0153L27.1066 30.069C25.3012 31.2062 23.0278 31.8181 20.4553 31.8181Z" fill="#34A853"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M39.5731 20C39.5731 18.8181 39.3909 17.5453 39.1178 16.3637H20.4553V24.0909H31.1978C30.6606 26.7256 29.1988 28.7509 27.1066 30.0691L33.5047 35.0153C37.1816 31.6028 39.5731 26.5191 39.5731 20Z" fill="#4285F4"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_8_37">
+    <rect width="40" height="40" fill="white"/>
+    </clipPath>
+    </defs>
+    </svg>
+    </div>
+    <div style={{width: 236, left: 69, top: 25, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Sign In with Google</div>
+  </div>
+  <div style={{width: 337, height: 69, left: 28, top: 517, position: 'absolute'}}>
+    <div style={{width: 337, height: 69, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 50, border: '1px #B9B1B1 solid'}} />
+    <div style={{width: 236, left: 69, top: 25, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Sign In with Facebook</div>
+    <div data-svg-wrapper style={{left: 18, top: 15, position: 'absolute'}}>
+    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 29.9826 7.31375 38.2566 16.875 39.757V25.7812H11.7969V20H16.875V15.5937C16.875 10.5812 19.8609 7.8125 24.4293 7.8125C26.6175 7.8125 28.9062 8.20312 28.9062 8.20312V13.125H26.3843C23.8998 13.125 23.125 14.6666 23.125 16.2483V20H28.6719L27.7852 25.7812H23.125V39.757C32.6863 38.2566 40 29.9826 40 20Z" fill="#1877F2"/>
+    <path d="M27.7852 25.7812L28.6719 20H23.125V16.2483C23.125 14.6666 23.8998 13.125 26.3843 13.125H28.9062V8.20312C28.9062 8.20312 26.6174 7.8125 24.4293 7.8125C19.8609 7.8125 16.875 10.5813 16.875 15.5938V20H11.7969V25.7812H16.875V39.757C17.8932 39.9168 18.9369 40 20 40C21.0631 40 22.1067 39.9168 23.125 39.757V25.7812H27.7852Z" fill="white"/>
+    </svg>
+    </div>
+  </div>
+  <div style={{width: 337, height: 69, left: 27, top: 608, position: 'absolute'}}>
+    <div style={{width: 337, height: 69, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 50, border: '1px #B9B1B1 solid'}} />
+    <div style={{width: 155, height: 12, left: 110, top: 26, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Sign In with Apple</div>
+    <div data-svg-wrapper style={{left: 14, top: 7, position: 'absolute'}}>
+    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M36.8449 26.9143C36.7903 21.418 41.327 18.7819 41.5298 18.6487C38.9801 14.9207 35.0079 14.4092 33.5941 14.351C30.2163 14.0079 26.9998 16.3406 25.2862 16.3406C23.5761 16.3406 20.9285 14.4012 18.1275 14.4506C14.4427 14.5053 11.0463 16.5938 9.15016 19.8922C5.32341 26.5306 8.17121 36.3687 11.9001 41.7565C13.723 44.39 15.897 47.3533 18.7492 47.2448C21.4982 47.1363 22.5354 45.4668 25.8586 45.4668C29.1817 45.4668 30.1148 47.2448 33.0226 47.1892C35.9789 47.1354 37.853 44.5055 39.661 41.8614C41.7538 38.802 42.6155 35.8395 42.6666 35.6896C42.6023 35.6596 36.904 33.4768 36.8449 26.9143Z" fill="black"/>
+    <path d="M31.3813 10.7853C32.8938 8.94906 33.9187 6.3976 33.64 3.8576C31.4572 3.9458 28.8149 5.3084 27.2476 7.14284C25.8436 8.77091 24.6159 11.3629 24.9449 13.8571C27.3799 14.0458 29.8635 12.6179 31.3813 10.7853Z" fill="black"/>
+    </svg>
+    </div>
+  </div>
+  <div style={{width: 337, height: 69, left: 28, top: 270, position: 'absolute'}}>
+    <div style={{width: 337, height: 69, left: 0, top: 0, position: 'absolute', background: '#B9B1B1', borderRadius: 50, border: '1px #B9B1B1 solid'}} />
+    <div style={{width: 93.01, left: 122, top: 25, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Continue</div>
+  </div>
+  <div style={{width: 337, height: 69, left: 28, top: 179, position: 'absolute'}}>
+    <div style={{width: 337, height: 69, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 50, border: '1px #B9B1B1 solid'}} />
+    <div style={{width: 93.01, left: 36.24, top: 25, position: 'absolute', color: '#B9B1B1', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Password</div>
+  </div>
+  <div style={{left: 69, top: 351, position: 'absolute', textAlign: 'center', color: '#B9B1B1', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Do not have an account? Register</div>
+  <div data-svg-wrapper style={{left: 341, top: 24, position: 'absolute'}}>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8.99999 7.58579L12 10.5858L15 7.58579L16.4142 9L13.4142 12L16.4142 15L15 16.4142L12 13.4142L8.99999 16.4142L7.58578 15L10.5858 12L7.58578 9L8.99999 7.58579Z" fill="black"/>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3Z" fill="black"/>
+  </svg>
+  </div>
+</div>
     </div>
   );
 }
